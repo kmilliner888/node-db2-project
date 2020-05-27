968f285050918d8ca8f7dce240f4d0a0b5ee48ce
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-05-26 21:21:24 -0400

    connected sqlite3 database and created endpoints

---
+++ index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const server = require('./server');
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+    console.log(`listening on localhost: ${port}`)
+});
